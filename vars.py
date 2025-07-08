5726ca424bed173d5672a64b1a39a32d3fdb1c7b
raghav5761 <raghavgargtg@gmail.com>
2025-07-08 17:10:41 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,7 @@
 api_id = 29993012 #change_with_your_Api_id.
 api_hash = "48f4468d1f05c8f6b5c99c75f7810988" #change_with_your_Api_hash.
 bot_token = "8128912582:AAGbQqbYpje3RFZbpCf4bF8Scb7-08L4xRw" #change_with_your_bot_token.
-auth_users = [] #change_with_your_user_id.
-sudo_user =  #change_with_your_user_id.
-log_channel = #change_with_your_channel_id.
-txt_channel =  #change_with_your_channel_id.
+auth_users = [9521442149] #change_with_your_user_id.
+sudo_user =  1713924419 #change_with_your_user_id.
+log_channel = -1002154281532 #change_with_your_channel_id.
+txt_channel =  -1002154281532 #change_with_your_channel_id.
