5e4a36de7d82a3a2c3cbff1e49f0612d2f324e35
raghav5761 <raghavgargtg@gmail.com>
2025-07-08 17:03:55 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,7 @@
-api_id = 35814071 #change_with_your_Api_id.
-api_hash = "80c231f0e8704995b8259d06965n4351" #change_with_your_Api_hash.
-bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
-auth_users = [999741495] #change_with_your_user_id.
-sudo_user = 1713924419 #change_with_your_user_id.
-log_channel = -1002154281532 #change_with_your_channel_id.
-txt_channel = -1002154281532 #change_with_your_channel_id.
+api_id = 29993012 #change_with_your_Api_id.
+api_hash = "48f4468d1f05c8f6b5c99c75f7810988" #change_with_your_Api_hash.
+bot_token = "8128912582:AAGbQqbYpje3RFZbpCf4bF8Scb7-08L4xRw" #change_with_your_bot_token.
+auth_users = [] #change_with_your_user_id.
+sudo_user =  #change_with_your_user_id.
+log_channel = #change_with_your_channel_id.
+txt_channel =  #change_with_your_channel_id.
